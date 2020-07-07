b3eb2061bb821097bc5f2c03c4eaf22456f22f66
Evan Merzon <evandaniel160@yahoo.com>
2020-07-07 19:02:43 -0400

    fixed reverseString.js

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    let res = ""
+    for(let i = s.length-1; i >= 0; i --){
+        res += s.charAt(i);
+    }
+    return res;
 }
 
 module.exports = reverseString
