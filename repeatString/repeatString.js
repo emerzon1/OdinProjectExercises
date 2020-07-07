98e03c0c11e14eb7dd89566ce41f553e3919d697
Evan Merzon <evandaniel160@yahoo.com>
2020-07-07 18:09:23 -0400

    Set up files

---
+++ repeatString/repeatString.js
@@ -0,0 +1,10 @@
+const repeatString = function(str, num) {
+    if(num<0)return "ERROR";
+    let res = "";
+    for(let i = 0; i < num ; i ++){
+        res += str;
+    }
+    return res;
+}
+
+module.exports = repeatString
