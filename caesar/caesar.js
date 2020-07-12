c814176bf2e558033bd41ed9da975a8eb0e1d62d
Evan Merzon <evandaniel160@yahoo.com>
2020-07-12 19:24:55 -0400

    fixed caesar

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,20 @@
-const caesar = function() {
+const caesar = function(str, num) {
+    let res = "";
+    let lower = "abcdefghijklmnopqrstuvwxyz";
+    let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for(let i = 0; i < str.length; i ++){
+        if(!((str.charCodeAt(i) >= 65 && str.charCodeAt(i) < 91) || (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122))){
+            res += str[i];
+        }
+        else if(str[i] == str[i].toLowerCase()){
+            res += lower[Math.abs((str.charCodeAt(i) - 97) + num + 2600) %26];
+        }
+        else{
+            res+= upper[Math.abs((str.charCodeAt(i) - 65) + num + 2600)%26];
+        }
+    }
 
+    return res;
 }
 
 module.exports = caesar
