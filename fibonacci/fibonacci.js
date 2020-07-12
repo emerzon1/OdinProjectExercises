025b515a19d02fbefe29f6bf793b33d104be8ca6
Evan Merzon <evandaniel160@yahoo.com>
2020-07-12 19:45:44 -0400

    solved fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    a = parseInt(a);
+    if(a<0){
+        return "OOPS";
+    }
+    if(a<=2){
+        return 1;
+    }
+    let prev = 1;
+    let curr = 1;
+    for(let i = 2; i < a; i ++){
+        var temp = curr;
+        curr += prev;
+        prev = temp;
+    }
+    return curr;
 }
 
 module.exports = fibonacci
