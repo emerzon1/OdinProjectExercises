11c50fb82b8a5f9a6a28605d9760838e89db1c09
Evan Merzon <evandaniel160@yahoo.com>
2020-07-13 08:25:02 -0400

    solved findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(arr) {
+    return arr.reduce((a, b) => {
+        if(a.yearOfDeath==null){
+            a.yearOfDeath = 2020;
+        }
+        if(b.yearOfDeath == null){
+            b.yearOfDeath = 2020;
+        }
+        if((a.yearOfDeath-a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)){
+            return a;
+        }
+        else{
+            return b;
+        }
+    })
 }
 
 module.exports = findTheOldest
